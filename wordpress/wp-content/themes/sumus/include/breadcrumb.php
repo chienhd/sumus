b0379299abde3cb3b650c007c0135d1cecc5aa11
chienhd <chienhd@sudobo.com>
2021-07-29 22:16:59 +0700

    git cache cleared

--- wordpress/wp-content/themes/sumus/include/breadcrumb.php
+++
@@ -1,9 +0,0 @@
-<div class="breadcrumb">
-<span class="home"><a href="/"><span class="icon-home"></span></a></span>
->
-<?php
-	if(function_exists('bcn_display')) {
-		bcn_display();
-	}
-?>
-</div>
